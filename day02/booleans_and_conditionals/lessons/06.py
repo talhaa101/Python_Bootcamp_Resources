0fae3caa149c02e9e752eaa2c6d47df2c4e78b6f
Abu Talha <talha007abu@gmail.com>
2026-03-10 23:31:43 +0600

    day 02 resources

---
+++ day02/booleans_and_conditionals/lessons/06.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+age = 40
+if age >= 35:
+    print('You are old enough to be a Senator or the President.')#
+if age >=39:
+    print('You are old enough to be the President.')
+
+
+if age >= 38: # check kore
+    print('You are old enough to be the President.')
+elif age >= 30: 
+    print('You are old enough to be a Senator.')
+
+else: 
+    print('You are not old enough to be a Senator or the President.')
+
+print('Have a nice day!')#
---
+++ day02/numbers/lessons/09.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# The following code:
+#     Computes the hosting costs for one server.
+#     Determines the duration of hosting that can be purchased given a budget.
+
+
+
+# this is a demo print statement
+print("this is a pen")
