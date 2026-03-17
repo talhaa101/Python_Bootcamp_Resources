7933d4766f6054032736c2e014d6c3cfbfc31ec0
Abu Talha <talha007abu@gmail.com>
2026-03-17 23:47:03 +0600

    bootcamp done

---
+++ day05/tuples/lessons/05.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+days_of_the_week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+(a, tue, wed, thr, b, sat, sun) = days_of_the_week
+print(a)
+print(b)
